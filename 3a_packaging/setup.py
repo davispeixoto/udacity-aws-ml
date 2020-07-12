ce6029ce4b89310fd213cae692469e8a4d8cc142
Davis Peixoto <davis.peixoto@gmail.com>
2020-07-12 12:22:25 -0300

    Packaging
    Signed-off-by: Davis Peixoto <davis.peixoto@gmail.com>

---
+++ 3a_packaging/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='distributions',
+    version='1.0.0',
+    author='Davis Peixoto',
+    url='https://github.com/davispeixoto/udacity-aws-ml',
+    author_email='davis.peixoto@gmail.com',
+    description='Udacity AWS ML Course',
+    packages=find_packages(),
+)
