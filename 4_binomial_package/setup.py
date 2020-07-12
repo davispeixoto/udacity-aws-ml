67b6606b2800f1eeb4341f273e49662c142a4cee
Davis Peixoto <davis.peixoto@gmail.com>
2020-07-12 18:30:40 -0300

    Binomial class ... my version, passing tests.. missing the plot pdf bar
    Signed-off-by: Davis Peixoto <davis.peixoto@gmail.com>

---
+++ 4_binomial_package/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='distributions',
+    version='1.0.0',
+    author='Davis Peixoto',
+    url='https://github.com/davispeixoto/udacity-aws-ml',
+    author_email='davis.peixoto@gmail.com',
+    description='Udacity AWS ML Course',
+    packages=find_packages(),
+    # packages=['distributons'],
+    zip_safe=False
+)
